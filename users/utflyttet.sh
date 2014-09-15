6fe09e6054d54cd7d1bbf35d1933b1eda308f95e
Henrik Steen <henrist@henrist.net>
2014-09-15 23:20:21 +0200

    Script for å sette brukere som utflyttet. Fra https://github.com/blindern/printerserver/blob/master/scripts/utflyttet

---
+++ users/utflyttet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This script removes the user from 'beboer'-group,
+# and all other groups specified on command line, and
+# adds the user to 'utflyttet'-group
+
+user=${1}
+if [ "$user" == "" ];
+then
+	echo "Missing user!"
+	exit 1
+fi
+
+ldapdeleteuserfromgroup $user beboer
+for x in ${*:2}
+do
+	ldapdeleteuserfromgroup $user $x
+done
+
+ldapaddusertogroup $user utflyttet
