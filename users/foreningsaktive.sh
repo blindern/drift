3e3509e222e88b3a91d71bd864d9695cce55ad33
Henrik Steen <henrist@henrist.net>
2014-09-15 23:32:39 +0200

    Script for å legge brukere som direkte medlemmer av foreningsaktive-gruppa.

---
+++ users/foreningsaktive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script makes all users that is indirectly
+# a member of the group 'foreningsaktive'
+# a direct member of the group
+
+# It will post a "error" for all users that is already
+# a direct member of the group, while it will correctly
+# add those who are not
+
+getent group foreningsaktive | awk -F: '{print $4}' | tr ',' '\n' | xargs -iX ldapaddusertogroup X foreningsaktive
