164356a17c83e5c9884c2e8241d3f161002d36cc
Henrik Steen <henrist@henrist.net>
2016-08-17 07:10:29 +0200

    Scripts for comparison against postliste

---
+++ users/check_users.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import difflib
+import json
+import sys
+import urllib.request
+
+def getUsers():
+    with urllib.request.urlopen('https://foreningenbs.no/users-api/users?grouplevel=1') as response:
+        data = response.read().decode('utf-8')
+    data = json.loads(data)
+
+    result = []
+    for user in data['result']:
+        result.append({
+            'username': user['username'],
+            'realname': user['realname'],
+            'groups': list(user['groups_relation'].keys()) if len(user['groups_relation']) > 0 else []
+        })
+
+    return result
+
+def findMatch(lookupValue, listOfNames):
+    closeMatches = difflib.get_close_matches(lookupValue, listOfNames, n=2, cutoff=0.6)
+    if len(closeMatches) == 0:
+        return False
+
+    # We need a stricter match, but we might miss e.g. a middle name
+    # We do the strict match by looking at each possible match, picking
+    # the shortest lengthed value of either the possible match or original
+    # lookup value and looking up to check if every word is represented
+    # in the longer variant, with some tollerance to misspelling
+    for item in closeMatches:
+        if len(item) > len(lookupValue):
+            shortname = lookupValue.replace('.', '')
+            longname = item.replace('.', '')
+        else:
+            shortname = item.replace('.', '')
+            longname = lookupValue.replace('.', '')
+
+        found = True
+        for word in shortname.split():
+            if difflib.get_close_matches(word, longname.split(), n=1, cutoff=0.9):
+                continue
+
+            # take into account that the name may be abbreviated
+            foundlw = False
+            for lw in longname.split():
+                if word in lw:
+                    foundlw = True
+                    break
+
+            if not foundlw:
+                found = False
+                break
+
+        if found:
+            return item
+
+    return False
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Syntax: %s <beboerliste.json>" % sys.argv[0])
+        sys.exit(1)
+
+    with open(sys.argv[1], 'r') as f:
+        people = sorted(json.load(f), key=lambda person: '%s %s' % (person['firstname'], person['lastname']))
+
+    users = getUsers()
+    users_names = [user['realname'] for user in users]
+
+    people_found = []
+    users_found = []
+
+    for person in people:
+        name = '%s %s' % (person['firstname'], person['lastname'])
+
+        match = findMatch(name, users_names)
+        if match == False:
+            continue
+        if name != match:
+            print("Found similar name %-30s registered as %s" % (name, match))
+
+        people_found.append(person)
+
+        user = None
+        for elm in users:
+            # there might be multiple entries.. doh
+            if elm['realname'] == match:
+                users_found.append(elm)
+                user = elm
+        if 'beboer' not in user['groups']:
+            print("Moved back in? %s (%s)" % (name, user['username']))
+
+    for person in people:
+        if person not in people_found:
+            print("Unregistered person: %s %s" % (person['firstname'], person['lastname']))
+
+    for user in users:
+        if 'beboer' in user['groups'] and user not in users_found:
+            print("./utflyttet.sh %-20s  # %s" % (user['username'], user['realname']))
---
+++ users/postliste.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import collections
+import csv
+import json
+import os
+import re
+import subprocess
+import sys
+import tempfile
+
+class PostlisteParser():
+    employeeRooms = [
+        '207',  # husfar
+        '201',  # daglig leder
+        '202',  # kjøkkenet
+        '203',  # oldfrue
+        '200',  # resepsjonen
+        '206',  # vedlikehold
+    ]
+
+    @staticmethod
+    def parseName(name):
+        # the list often misses a space after the comma
+        name = re.sub(',([^ ])', ', \\1', name)
+        name = re.sub('  +', ' ', name).strip()
+
+        # warn if missing comma
+        if ',' not in name:
+            print('Malformed name: %s' % name, file=sys.stderr)
+
+        name = name.split(', ', 1)
+        lastname = name[0]
+        firstname = name[1] if len(name) > 0 else ''
+
+        return (lastname, firstname)
+
+    @staticmethod
+    def parseRoom(room):
+        room = room.replace(' ', '')
+        return room
+
+    def __init__(self):
+        self.people = []
+
+    def parseExcelFile(self, excelFile):
+        tmp = tempfile._get_default_tempdir() + os.path.sep + 'beboere.csv'
+        self.convertToCsv(excelFile, tmp)
+        self.people = self.loadFromCsv(tmp)
+        os.remove(tmp)
+
+    @staticmethod
+    def convertToCsv(excelfile, outfile):
+        ret = subprocess.call(["ssconvert", excelfile, outfile],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+
+        if ret != 0:
+            print("ssconvert failed, do you have it?", file=sys.stderr)
+            sys.exit(1)
+
+    def loadFromCsv(self, csvFile):
+        people = []
+
+        with open(csvFile, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # skip header
+            for row in reader:
+                for coli in range(0, len(row)):
+                    if coli % 2 == 0:
+                        continue
+                    name = row[coli-1]
+                    room = PostlisteParser.parseRoom(row[coli])
+
+                    if name.strip() == '' or 'ADMINIS' in name or 'PORTEN' in name \
+                                          or room in self.employeeRooms:
+
+                        continue
+
+                    name = PostlisteParser.parseName(name)
+
+                    for person in people:
+                        if name[0] == person['lastname'] and name[1] == person['firstname']:
+                            print("Found duplicate entry of %s %s - skipping" % (name[1], name[0]), file=sys.stderr)
+                            break
+
+                    people.append({
+                        'firstname': name[1],
+                        'lastname': name[0],
+                        'room': room
+                    })
+
+        people = sorted(people, key=lambda person: person['lastname'] + person['firstname'])
+        return people
+
+    def storeAsJson(self, fileName):
+        with open(fileName, 'w') as f:
+            json.dump(self.people, f)
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing input file (Excel file to parse)")
+        print("Syntax: %s <inputfile.xls> [<outputfile.json>]" % sys.argv[0])
+        sys.exit(1)
+
+    parser = PostlisteParser()
+    parser.parseExcelFile(sys.argv[1])
+
+    if len(sys.argv) > 2:
+        with open(sys.argv[2], 'w') as f:
+            json.dump(parser.people, f, indent=2)
+            print("Found %d people. Save to %s" % (len(parser.people), sys.argv[2]))
+    else:
+        print(json.dumps(parser.people, indent=2))
