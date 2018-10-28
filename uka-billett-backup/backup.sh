36f30489a0973534c86f32270ef9290e61c2ba71
Henrik Steen <henrist@henrist.net>
2018-10-28 04:00:53 +0100

    Update backup script for uka-billett

--- uka-billett-backup/backup.sh
+++ uka-billett-backup/backup.sh
@@ -8,8 +8,8 @@ set -e
 out=/fbs/drift/uka-billett-backup/uka_billett_backup_$(date +%Y%m%d.%H%M%S).sql
 
 ssh -q -tt -p 2222 core@server2016.blindernuka.no '
-  cd ~/billett
-  /opt/bin/docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml exec database sh -c "mysqldump --password=\$MYSQL_ROOT_PASSWORD uka_billett"
+  cd ~/infrastructure/server-2018
+  /opt/bin/docker-compose exec database sh -c "mysqldump --password=\$MYSQL_ROOT_PASSWORD uka_billett"
   ' >"$out"
 
 gzip -9 "$out"
