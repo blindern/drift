9f0185d0f762420faac4d88b4775bf7bea645336
Henrik Steen <henrist@henrist.net>
2022-06-06 03:25:58 +0200

    Rewrite backup script for Confluence

--- backup/confluence.sh
+++ backup/confluence.sh
@@ -7,17 +7,17 @@ set -eu -o pipefail
 echo "Taking backup of confluence..."
 date
 
-confluence_server=coreos-3.foreningenbs.no
+confluence_server=fcos-1.nrec.foreningenbs.no
 
-confluence_data_dir=/data/confluence-data
-confluence_logs_dir=/data/confluence-logs
+confluence_data_dir=/var/mnt/data/confluence-data
+confluence_logs_dir=/var/mnt/data/confluence-logs
 
-# not used since its the same server - for simplification
-db_server=coreos-3.foreningenbs.no
+db_server=fcos-2.nrec.foreningenbs.no
 db_container_name=mysql-1
 
 timestamp=$(date -u +%Y%m%d-%H%M%S)Z
-backup_dir=/data/confluence-backup-staging/$timestamp
+backup_dir=/var/mnt/data/confluence-backup-staging/$timestamp
+out_dir="confluence-backup-$timestamp"
 
 echo "Running external commands"
 
@@ -33,20 +33,37 @@ ssh root@$confluence_server <<EOF
   cd "$confluence_logs_dir"
   tar zcf "$backup_dir/logs.tgz" .
 
+  echo "Extracting database password"
+  MYSQL_PWD=\$(grep hibernate.connection.password /var/mnt/data/confluence-data/confluence.cfg.xml | cut -d'>' -f2 | cut -d'<' -f1)
+  echo "\$MYSQL_PWD" >"$backup_dir/mysql-password.txt"
+EOF
+
+echo "Fetching data (1/2)"
+scp -r root@$confluence_server:"$backup_dir" "$out_dir"
+
+mysql_pwd=$(cat "$out_dir/mysql-password.txt")
+
+ssh root@$db_server <<EOF
+  echo "Creating directory for backup"
+  mkdir -p "$backup_dir"
+
   echo "Creating database dump"
-  export MYSQL_PWD=\$(grep hibernate.connection.password /data/confluence-data/confluence.cfg.xml | cut -d'>' -f2 | cut -d'<' -f1)
+  export MYSQL_PWD="$mysql_pwd"
   docker exec -i -e MYSQL_PWD $db_container_name mysqldump -u confluence --routines --max_allowed_packet=512M --single-transaction confluence | gzip -9 >"$backup_dir/db.sql.gz"
 EOF
 
-echo "Fetching data"
-
-scp -r root@$confluence_server:"$backup_dir" "confluence-backup-$timestamp"
+echo "Fetching data (2/2)"
+scp -r root@$confluence_server:"$backup_dir/*" "$out_dir/"
 
 ls -lah "confluence-backup-$timestamp"
 
 echo "Removing from server"
 
 ssh root@$confluence_server <<EOF
+  rm -rf "$backup_dir"
+EOF
+
+ssh root@$db_server <<EOF
   rm -rf "$backup_dir"
 EOF
 
